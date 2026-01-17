71c90918813e1fb8794e8b7fec73ce73911b5f28
Sandacus <sandyarl329@gmail.com>
2026-01-17 21:30:38 +0000

    refactored get_subscriber and init_subscriber into telemetry module

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,9 @@ use std::net::TcpListener;
 
 use sqlx::PgPool;
 use tracing::Subscriber;
-use zero2prod::{configuration::get_configuration, startup::run};
-
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
-
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
-    LogTracer::init().expect("Failed to set logger");
-    set_global_default(subscriber).expect("Failed to set subscriber");
-}
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
