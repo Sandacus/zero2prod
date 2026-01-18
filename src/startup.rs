842e69f2fbd486957bef06e6109037ab40d52ed1
Sandacus <sandyarl329@gmail.com>
2026-01-18 21:34:25 +0000

    added tracing-actix-web crate that is a tracing aware replacement for actix webs logger

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,9 @@
 //! src/startup.rs
 
-use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, dev::Server, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -14,7 +14,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route(HEALTH_CHECK, web::get().to(health_check))
             .route(SUBSCRIBE, web::post().to(subscribe))
             .app_data(db_pool.clone())
