3f3b4c107250b4ec8becf57e1202dbfad62d24b8
Sandacus <sandyarl329@gmail.com>
2026-01-18 18:14:04 +0000

    added secrecy crate to stop logging of secrets. Added the version in line with book. TODO update to latest version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::TcpListener;
-
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,9 +12,10 @@ async fn main() -> Result<(), std::io::Error> {
 
     // for now, panic if we can't read the configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let address = format!("127.0.0.1:{}", configuration.application_port);
